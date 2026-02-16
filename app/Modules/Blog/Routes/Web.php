c2f954ea89dd098fd25d028708069222885bf4e2
Shubhkamna <krishna.shubhventures@gmail.com>
2026-02-16 23:15:02 +0530

    '260216'

--- app/Modules/Blog/Routes/Web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Modules\Blog\Controllers\HomeController;
-Route::prefix('blog')->group(function () {
-    Route::get('/', [HomeController::class, 'index']);
-});
--- resources/views/pages/home/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends("layouts.frontend")
-
-@section("content")
-
-    <section class="min-vh-100">
-hi
-    </section>
-
-@endsection
